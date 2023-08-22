928d97e611548340b95c9a766205ac49c1de474e
miguelangelacosta <113574101+miguelangelacosta@users.noreply.github.com>
2023-08-21 22:33:12 -0500

    tienda001

--- src/components/Banner/BannerBottom.js
+++ src/components/Banner/BannerBottom.js
@@ -7,20 +7,20 @@ const BannerBottom = () => {
     <div className="w-full bg-white border-b-[1px] py-4 border-b-gray-200 px-4">
       <div className="max-w-container mx-auto h-20 flex flex-col md:flex-row justify-between items-center">
         <div className="flex items-center gap-2 w-72 shadow-sm hover:shadow-md duration-300">
-          <span className="font-bold font-titleFont w-6 text-center">2</span>
-          <p className="text-lightText text-base">Two years warranty</p>
+          <span className="font-bold font-titleFont w-6 text-center">escribenenos 3175533775 para mas info  </span>
+          <p className="text-lightText text-base"></p>
         </div>
         <div className="flex md:w-auto items-center gap-2 w-72 shadow-sm hover:shadow-md duration-300">
           <span className="text-xl text-center w-6 ml-1">
             <MdLocalShipping />
           </span>
-          <p className="text-lightText text-base">Free shipping</p>
+          <p className="text-lightText text-base">servio domicilio</p>
         </div>
         <div className="flex md:w-auto items-center gap-2 w-72 shadow-sm hover:shadow-md duration-300">
           <span className="text-2xl text-center w-6">
             <CgRedo />
           </span>
-          <p className="text-lightText text-base">Return policy in 30 days</p>
+          <p className="text-lightText text-base">compra con tranqulidad pagos contra entrega</p>
         </div>
       </div>
     </div>
